6ec6ee1cb62b154e41e269f9d1e5a2664dfa70ee
Daniel Straus <strausd@me.com>
2017-03-13 20:43:32 +0000

    Add connect and disconnect even handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
 const express = require("express");
 var app = express();
+
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.render('index');
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    })
 });
 
-
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.log('Server started...');
 });
