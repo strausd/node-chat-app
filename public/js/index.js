bef321d6dceb32f75794448fb8baad00c2a86715
Daniel Straus <strausd@me.com>
2017-03-16 21:42:19 +0000

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,19 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('New message received', message);
+    var li = document.createElement('LI');
+    li.innerHTML = `${message.from}: ${message.text}`;
+    document.querySelector('#messages').appendChild(li);
+});
+
+document.getElementById('message-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    socket.emit('createMessage', {
+        from: 'User',
+        text: document.getElementsByName('message')[0].value
+    }, function() {
+        
+    });
+    document.getElementsByName('message')[0].value = '';
 });
--- server/server.js
+++ server/server.js
@@ -19,16 +19,17 @@ io.on('connection', (socket) => {
     
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
     
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('New message created', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
         
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
         //     createdAt: new Date().getTime()
         // });
-    })
+    });
     
     socket.on('disconnect', () => {
         console.log('User was disconnected');
